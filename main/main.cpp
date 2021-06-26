a82838f15bfe00a97cd9e901887d1b86f77b8a7c
hannes <33062605+HannesGitH@users.noreply.github.com>
2021-06-26 13:23:13 +0200

    ENDLICH! showing_buf_1 had to be true

--- main/main.cpp
+++ main/main.cpp
@@ -11,8 +11,9 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
+#include "freertos/semphr.h"
+#include "led_strip.h"
 
-#include "ws2812.hpp"
 
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
@@ -22,15 +23,40 @@
 #define DATA_PIN GPIO_NUM_12
 
 
+#define LED_STRIP_LENGTH 14U
+#define LED_STRIP_RMT_INTR_NUM 19U
+
+
+static struct led_color_t led_strip_buf_1[LED_STRIP_LENGTH];
+static struct led_color_t led_strip_buf_2[LED_STRIP_LENGTH];
+
+xSemaphoreHandle sema = xSemaphoreCreateBinary();
+
 void init_stuff(){
-    uint strip_length = 14;
-    Ws2812 ws = Ws2812(DATA_PIN);
-    irgb_t RGBs[strip_length]{0};
-    for(int i=0; i<strip_length;++i){
-        RGBs[i]=iRGB(255,17,123);
+    printf("initializing.");
+    static struct led_strip_t led_strip = {
+        .rgb_led_type = RGB_LED_TYPE_WS2812,
+        .led_strip_length = LED_STRIP_LENGTH,
+        .gpio = DATA_PIN,
+
+        .rmt_channel = RMT_CHANNEL_3,
+        .rmt_interrupt_num = LED_STRIP_RMT_INTR_NUM,
+        .showing_buf_1 = true,
+        .led_strip_buf_1 = led_strip_buf_1,
+        .led_strip_buf_2 = led_strip_buf_2,
+        .access_semaphore = sema
+    };
+    printf(".");
+    bool led_init_ok = led_strip_init(&led_strip);
+    if (led_init_ok){
+        printf("\nled strip initialized\n");
     }
-    ws.setLEDs(RGBs,strip_length);
-    ws.update();
+    printf("setting pixel\n");
+    led_strip_set_pixel_rgb(&led_strip,3,250,0,120);
+    printf("showing pixel\n");
+    led_strip_show(&led_strip);
+    printf("%d\n",(&led_strip)->rmt_channel);
+    printf("init done\n");
 }
 
 extern "C"{
